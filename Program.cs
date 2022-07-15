b0c0bc41a5f717c91bd0d1fedc71192bd75de76d
yisi2534 <67554572+yisi2534@users.noreply.github.com>
2022-07-15 17:52:58 +0300

    Assignment3

--- Program.cs
+++ Program.cs
@@ -1,51 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Program
 {
-    internal class Program
+    abstract class Shape
     {
-        static void Main(string[] args)
+        public abstract double calculateArea();
+        public abstract double calculatePerimeter();
+    }
+
+    class Circle : Shape
+    {
+        static readonly double PI = 3.14;
+        public double Radius { get; set; }
+
+        public override double calculateArea()
         {
-            int i = 0, num = 0;
-            Console.WriteLine("Enter the number of students: ");
-            num = int.Parse(Console.ReadLine());
-            while (i < num)
-            {
-                Console.WriteLine("Enter mark ");
-                double mark = double.Parse(Console.ReadLine());
-                if (mark < 100 && mark > 90)
-                {
-                    Console.WriteLine("A");
-                    Console.ReadLine();
-                }
-                else if (mark < 90 && mark > 80)
-                {
-                    Console.WriteLine("B");
-                    Console.ReadLine();
-                }
-                else if (mark < 80 && mark > 70)
-                {
-                    Console.WriteLine("C");
-                    Console.ReadLine();
-                }
-                else if (mark < 70 && mark > 60)
-                {
-                    Console.WriteLine("D");
-                    Console.ReadLine();
-                }
-                else if (mark < 60 && mark > 0)
-                {
-                    Console.WriteLine("F");
-                    Console.ReadLine();
-                }
-                else
-                    Console.WriteLine("incorrect entry");
-                i++;
-            }
+            return PI * Radius * Radius;
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2 * PI * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public override double calculateArea()
+        {
+            return Length * Width;
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+    class Lab3
+    {
+        static void Main()
+        {
+            Circle circle = new Circle();
+            Rectangle rect = new Rectangle();
+
+            circle.Radius = 2;
+            rect.Width = 3;
+            rect.Length = 5;
+
+            Console.WriteLine("Radius: " + circle.Radius + "\nArea: " + circle.calculateArea());
+            Console.WriteLine("\nLength; " + rect.Length + "\nWidth: " + rect.Width + "\n" +
+                              "Area: " + rect.calculateArea() + "\nPerimeter: " + rect.calculatePerimeter());
+
         }
     }
 }
